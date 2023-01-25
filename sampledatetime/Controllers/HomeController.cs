39d3337549dd1fb57a440e879ed29eeac0a3d020
Administrator <admin@example.com>
2023-01-25 07:48:23 +0000

    Update sampledatetime/Controllers/HomeController.cs

--- sampledatetime/Controllers/HomeController.cs
+++ sampledatetime/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace sampledatetime.Controllers
 {
-    public class HomeController : ontroller
+    public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
